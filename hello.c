754e8025d2a07310bd4a1b8fc8f9da77d19a81ac
ClairePan220 <54620749+ClairePan220@users.noreply.github.com>
2019-10-15 14:50:55 +0800

    Update hello.c

--- hello.c
+++ hello.c
@@ -7,7 +7,7 @@ CURLcode res;
 curl=curl_easy_init();
 if(curl){
   curl_easy_setopt(curl,CURLOPT_URL,"http://myip.ipip.net/");
-  res=curl-easy_perform(curl);
+  res=curl_easy_perform(curl);
   if(res!=CURLE_OK)
     fprintf(stderr,"curl_easy_perform() failed:%s\n",
       curl_easy_strerror(res));
